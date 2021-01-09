11d00b19606bcb6f3a7c3b8a8e0e3913a315592f
Engin Gökmen <engin.gokmen@clevel.team>
2021-01-09 12:10:54 +0300

    f

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -1,29 +1,61 @@
 <template>
   <div>
-    <h2>{{$t('FormTitle')}}</h2>
-    <form action="/example" method="post" @submit="checkForm" class="content form contact">
+    <h2>{{ $t('FormTitle') }}</h2>
+    <form
+      action="/example"
+      method="post"
+      @submit="checkForm"
+      class="content form contact"
+    >
       <div class="form-item">
-        <label for="name">{{$t('İsim')}}:</label>
-        <input type="text" name="name" id="name" v-model="name" required>
+        <label for="name">{{ $t('İsim') }}:</label>
+        <input type="text" name="name" id="name" v-model="name" required />
       </div>
       <div class="form-item">
-        <label for="email">{{$t('Eposta')}}: </label>
-        <input class="validated-field" type="email" name="email" id="email" v-model="email" required>
+        <label for="email">{{ $t('Eposta') }}: </label>
+        <input
+          class="validated-field"
+          type="email"
+          name="email"
+          id="email"
+          v-model="email"
+          required
+        />
       </div>
       <div class="form-item">
-        <label for="phone">{{$t('Telefon')}}: </label>
-        <input class="validated-field" type="tel" name="phone" id="phone" v-model="phone" placeholder="5321231212" pattern="[0-9]{10}" required>
+        <label for="phone">{{ $t('Telefon') }}: </label>
+        <input
+          class="validated-field"
+          type="tel"
+          name="phone"
+          id="phone"
+          v-model="phone"
+          placeholder="5321231212"
+          pattern="[0-9]{10}"
+          required
+        />
       </div>
       <div class="form-item">
-        <label for="country">{{$t('Ülke')}}: </label>
-        <vSelect v-model="country" :reduce="country => country.code" :options="countryList" />
+        <label for="country">{{ $t('Ülke') }}: </label>
+        <vSelect
+          v-model="country"
+          :reduce="country => country.code"
+          :options="countryList"
+        />
       </div>
       <div class="form-item">
-        <label for="text">{{$t('Açıklama')}}: </label>
-        <textarea type="text" name="text" id="text" rows="5" v-model="text" required></textarea>
+        <label for="text">{{ $t('Açıklama') }}: </label>
+        <textarea
+          type="text"
+          name="text"
+          id="text"
+          rows="5"
+          v-model="text"
+          required
+        ></textarea>
       </div>
       <div class="submit">
-        <input type="submit" :value="$t('Gönder')">
+        <input type="submit" :value="$t('Gönder')" />
       </div>
     </form>
   </div>
@@ -35,72 +67,64 @@ import vSelect from 'vue-select';
 import 'vue-select/dist/vue-select.css';
 
 const countryList = [
-	{ id: "TR", name: "Turkey" },
-	{ id: "US", name: "United States of America" },
-	{ id: "GB", name: "United Kingdom" },
-	{ id: "DE", name: "Germany" },
-	{ id: "SE", name: "Sweden" },
-	{ id: "KE", name: "Kenya" },
-	{ id: "BR", name: "Brazil" },
-	{ id: "ZW", name: "Zimbabwe" }
-]
+  { id: 'TR', name: 'Turkey' },
+  { id: 'US', name: 'United States of America' },
+  { id: 'GB', name: 'United Kingdom' },
+  { id: 'DE', name: 'Germany' },
+  { id: 'SE', name: 'Sweden' },
+  { id: 'KE', name: 'Kenya' },
+  { id: 'BR', name: 'Brazil' },
+  { id: 'ZW', name: 'Zimbabwe' }
+];
 export default {
   props: ['userInfo'],
-  components:{
+  components: {
     vSelect
   },
   data() {
     return {
-      name:'',
-      email:'',
-      phone:null,
-      country:'',
-      text:'',
-      countryList:countryList.map(x=>{return {code:x.id, label:x.name}})
-    }
+      name: this.userInfo.name,
+      email: this.userInfo.email,
+      phone: null,
+      country: '',
+      text: '',
+      countryList: countryList.map(x => {
+        return { code: x.id, label: x.name };
+      })
+    };
   },
   mounted() {
-    this.userInfoHandler(this.userInfo)
-    EventBus.$on('userInfo', payload => {
-      this.userInfoHandler(payload);
-    });
+    console.log(this.userInfo);
   },
   methods: {
-    checkForm: function (e) {
+    checkForm: function(e) {
       e.preventDefault();
       const payload = {
-        name:this.name,
-        email:this.email,
-        phonenumber:this.phone,
-        country_code:this.country,
-        text:this.text
-      }
-      
-      const request = new Request('/example.com', {method: 'POST', body: JSON.stringify(payload)});
-      fetch(request)
-        .then(response => console.log(response))
-      console.log("payload", payload);
-    },
-    userInfoHandler(userInfo) {
-      if(userInfo != null) {
-        this.name = userInfo.name;
-        this.email = userInfo.email;
-      } else {
-        this.name = this.userInfo.name;
-        this.email = this.userInfo.email;
-      }
-    },
+        name: this.name,
+        email: this.email,
+        phonenumber: this.phone,
+        country_code: this.country,
+        text: this.text
+      };
+
+      const request = new Request('/example.com', {
+        method: 'POST',
+        body: JSON.stringify(payload)
+      });
+      fetch(request).then(response => console.log(response));
+      console.log('payload', payload);
+    }
   }
-}
+};
 </script>
 
 <style scoped>
+.contact {
+  padding-left: 6rem;
+}
+@media screen and (max-width: 600px) {
   .contact {
-    padding-left: 6rem;
+    padding-left: 0;
   }
-  @media screen and (max-width: 600px) {
-    .contact {
-      padding-left: 0;
-    }
-  }
+}
 </style>
